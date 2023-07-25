ee4a268e8ade8e9e2c5cbc7e5315ed719bb8a8a1
Ajay-Dhangar <99037494+Ajay-Dhangar@users.noreply.github.com>
2023-07-25 09:23:12 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from 'react'
-import Home from './pages/home/Home'
 import { Routes, Route } from 'react-router'
-import About from './pages/about/About'
-import Project from "./pages/projects/Project";
+// import About from './pages/about/About'
+// import Project from "./pages/projects/Project";
 import PageRoutes from './PageRoutes';
-import Docs from './pages/docs/Docs';
-import Skill from "./pages/skills/Skill";
-import Contect from './pages/contect/Contect';
-import Feature from './pages/feature/Feature';
+// import Docs from './pages/docs/Docs';
+// import Skill from "./pages/skills/Skill";
+// import Contect from './pages/contect/Contect';
+// import Feature from './pages/feature/Feature';
+// import { Profile, Gallery } from "./component";
+// import Translate from './component/translater/Translate'
+// import Navbar from './component/navbar/Navbar';
 
-import { Profile, Gallery } from "./component";
-import Translate from './component/translater/Translate'
+// import Button from './testing/Button'
+import Home from './pages/home/Home'
+
 
 function App() {
   return (
@@ -18,7 +21,7 @@ function App() {
       <PageRoutes/>
       <Routes>
         <Route path='/Home' element={<Home />} />
-        <Route path='/About' element={<About />} />
+        {/* <Route path='/About' element={<About />} />
         <Route path='/Project' element={<Project/>}/>
         <Route path='/Docs' element={<Docs/>}/>
         <Route path='/Project' element={<Project/>}/>
@@ -27,11 +30,17 @@ function App() {
         <Route path='/Feature' element={<Feature/>}/>
         <Route path='/Profile' element={<Profile/>}/>
         <Route path='/Gallery' element={<Gallery/>}/>
-        <Route path='/Translate' element={<Translate/>}/>
+        <Route path='/Translate' element={<Translate/>}/> */}
       </Routes>
      
+      {/* <Button/>       */}
+
+   
+
+      
     </div>
   )
 }
+
 
 export default App
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,11 +2,30 @@ import React from 'react'
 
 import './Home.css'
 
+import ProImg from '../../images/pics/Ajay-Dhangar.jpg'
+
 function Home() {
   return (
     <div>
       <div className="content">
-        <p><b>Home:</b> The home page should be the first thing visitors see when they access your portfolio. It can include a brief introduction about yourself, your areas of expertise, and your passion for programming.</p>
+        <main className="main-home-container">
+          <img src={ProImg} className="image-home" alt="Ajay Dhangar" />
+          <p className="content-home">
+            Hey there! I'm Ajay Dhangar, a passionate Frontend Developer with a flair
+            for creating stunning web applications. Welcome to my personal portfolio,
+            where I bring to life my ideas and concepts through the power of React and
+            modern web technologies.
+            <br />
+            I'm a skilled web developer with over 3 years of experience, specializing in
+            creating stunning website and graphic designs. I use the latest technologies and
+            industry best practices to deliver custom solutions that meet your unique needs and
+            exceed your expectations. Let's work together to take your online presence to the
+            next level.
+
+            <button class="cta-button">Explore More</button>
+          </p>
+          
+        </main>
       </div>
     </div>
   )
